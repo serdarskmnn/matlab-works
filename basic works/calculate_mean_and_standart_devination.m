c3256191c7061b7aa3c52407ceb784800854bdcf
serdarskmnn <mserdarsokmen@gmail.com>
2025-05-15 00:14:57 +0300

    added calculate mean and standart deviation

---
+++ basic works/calculate_mean_and_standart_devination.m
@@ -0,0 +1,5 @@
+data = input('enter a sequence of numbers (ex. [5 10 15 20]): ');
+meandata= mean(data);
+stdDva = std(data);
+fprintf('mean: %.2f\n', meandata);
+fprintf('standart deviation: %.2f\n', stdDva);
